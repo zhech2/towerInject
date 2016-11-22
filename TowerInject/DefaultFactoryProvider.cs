c1d1c1f944323e75c8552ddbdbb74f1bbfe30f52
Justin Olson <justin.olson@avasis.com>
2016-11-22 14:34:20 -0700

    Reverting back parameter to CreateFactory to use IInstanceResolverProvider.  Added comments.

--- TowerInject/DefaultFactoryProvider.cs
+++ TowerInject/DefaultFactoryProvider.cs
@@ -1,8 +1,16 @@
 ﻿namespace TowerInject
 {
+    /// <summary>
+    /// Creates an instance of the Default IFactory.  The Default is the <see cref="ReflectionFactory"/>.
+    /// </summary>
     public class DefaultFactoryProvider : IFactoryProvider
     {
-        public IFactory CreateFactory(IContainer container)
+        /// <summary>
+        /// Creates the factory
+        /// </summary>
+        /// <param name="provider">The resolver for getting required <see cref="IInstanceResolvers"/>.</param>
+        /// <returns></returns>
+        public IFactory CreateFactory(IInstanceResolverProvider provider)
         {
             return new ReflectionFactory();
         }
--- TowerInject/IFactoryProvider.cs
+++ TowerInject/IFactoryProvider.cs
@@ -1,7 +1,15 @@
 ﻿namespace TowerInject
 {
+    // <summary>
+    /// Creates an instance of <see cref="IFactory"/>.
+    /// </summary>
     public interface IFactoryProvider
     {
-        IFactory CreateFactory(IContainer container);
+        /// <summary>
+        /// Creates the factory
+        /// </summary>
+        /// <param name="provider">The resolver for getting required <see cref="IInstanceResolvers"/>.</param>
+        /// <returns></returns>
+        IFactory CreateFactory(IInstanceResolverProvider provider);
     }
 }
